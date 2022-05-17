336e5676e2dd36f28511a7a9f57fd7f28d98d348
gness1804 <graham.nessler@gmail.com>
2022-05-17 14:01:58 -0500

    all tests passing

--- robot-simulator.ts
+++ robot-simulator.ts
@@ -26,13 +26,58 @@ export class Robot {
       && options.direction !== 'south'
       && options.direction !== 'east'
       && options.direction !== 'west') {
-        throw new InvalidInputError('Invalid direction: ' + options.direction);
+      throw new InvalidInputError('Invalid direction: ' + options.direction);
     }
     this._coordinates = [options.x, options.y];
     this._bearing = options.direction;
   }
 
   evaluate(instructions: string) {
-    throw new Error('Remove this statement and implement this function')
+    instructions.split('').forEach((instruction) => {
+      switch (this._bearing) {
+        case 'north':
+          if (instruction === 'R') {
+            this._bearing = 'east';
+          } else if (instruction === 'L') {
+            this._bearing = 'west';
+          } else if (instruction === 'A') {
+            // increase y by one
+            this._coordinates = [this._coordinates[0], this._coordinates[1] + 1];
+          }
+          break;
+        case 'east':
+          if (instruction === 'R') {
+            this._bearing = 'south';
+          } else if (instruction === 'L') {
+            this._bearing = 'north';
+          } else if (instruction === 'A') {
+            // increase x by 1
+            this._coordinates = [this._coordinates[0] + 1, this._coordinates[1]];
+          }
+          break;
+        case 'south':
+          if (instruction === 'R') {
+            this._bearing = 'west';
+          } else if (instruction === 'L') {
+            this._bearing = 'east';
+          } else if (instruction === 'A') {
+            // decrease y by 1
+            this._coordinates = [this._coordinates[0], this._coordinates[1] - 1];
+          }
+          break;
+        case 'west':
+          if (instruction === 'R') {
+            this._bearing = 'north';
+          } else if (instruction === 'L') {
+            this._bearing = 'south';
+          } else if (instruction === 'A') {
+            // decrease x by 1
+            this._coordinates = [this._coordinates[0] - 1, this._coordinates[1]];
+          }
+          break;
+        default:
+          break;
+      }
+    })
   }
 }
